e19e13c7d9812f43de5cf56314d3b93019fc2c16
Joey <imjoeydhillon@gmail.com>
2024-10-06 21:47:54 -0500

    fourth commit

--- lab19.cpp
+++ lab19.cpp
@@ -5,14 +5,92 @@
 
 using namespace std;
 
-struct Movie
+class Movie
 {
+private:
     string title;
     float rating;
     string comments;
     Movie *next;
+public:
+    Movie* build(string textFileName){
+        char anotherReview = 'y';
+        ifstream fin;
 
+        Movie* head = nullptr;
+        Movie* current = nullptr;
+        fin.open(textFileName);
+
+        if(!fin){
+            cout << "File opening failure" << endl;
+            return nullptr;
+        }
+
+        time_t clock;
+        srand(time(NULL));
+
+        while (fin)
+        {
+            Movie *temp = new Movie();
+            string comments;
+            float rating;
+
+            getline(fin, comments);
+
+            if(fin){
+                rating = (rand()%6);
+
+                // Creates our temporary movie object to be appended
+                temp->comments = comments;
+                temp->rating = rating;
+                temp->next = nullptr;
+
+                // Checks whether to append to the end or the begining
+                // Checks if the head is empty
+                if(!head){
+                    head = temp;
+                    current = head;
+                }else{
+                    current->next = temp;
+                    current = current->next;
+                }
+            }
+        }
+
+        return head;
+    }
+
+    void print(Movie* head){
+        float ave = average(head);
+        int count = 1;
+
+        // Prints review
+        while(head){
+            cout << "\t> Review #" << count << ": " << head->rating  << ": " << head->comments << endl;
+            head = head->next;
+            count++;
+        }
+        cout << "\t> Average: " << ave << endl;
+    }
+
+    float average(Movie* head){
+        float sum = 0;
+        int count = 0;
+
+        while(head){
+            sum += head->rating;
+            head = head->next;
+            count++;
+        }
+        return sum/count;
+    }   
+    ~Movie();
 };
+
+Movie::~Movie()
+{
+}
+
 
 // Function headers
 Movie* build();
@@ -30,78 +108,3 @@ int main(int argc, char const *argv[]){
 }
 
 // Builds a movie from use input
-Movie* build(){
-    char anotherReview = 'y';
-    ifstream fin;
-
-    Movie* head = nullptr;
-    Movie* current = nullptr;
-    fin.open(fileName);
-
-    if(!fin){
-        cout << "File opening failure" << endl;
-        return nullptr;
-    }
-
-    time_t clock;
-    srand(time(NULL));
-
-    while (fin)
-    {
-        Movie *temp = new Movie();
-        string comments;
-        float rating;
-
-        getline(fin, comments);
-
-        if(fin){
-            rating = (rand()%6);
-
-            // Creates our temporary movie object to be appended
-            temp->comments = comments;
-            temp->rating = rating;
-            temp->next = nullptr;
-
-            // Checks whether to append to the end or the begining
-                // Checks if the head is empty
-            if(!head){
-                head = temp;
-                current = head;
-            }else{
-                current->next = temp;
-                current = current->next;
-            }
-        }
-    }
-
-    return head;
-}
-
-void print(Movie* head){
-    float ave = average(head);
-    int count = 1;
-
-    // Prints review
-    while(head){
-        cout << "\t> Review #" << count << ": " << head->rating  << ": " << head->comments << endl;
-        head = head->next;
-        count++;
-    }
-    cout << "\t> Average: " << ave << endl;
-}
-
-float average(Movie* head){
-    float sum = 0;
-    int count = 0;
-
-    while(head){
-        sum += head->rating;
-        head = head->next;
-        count++;
-    }
-
-    return sum/count;
-}
-
-//Returns Movie at given index
-
